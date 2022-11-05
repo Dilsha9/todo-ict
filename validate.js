0763afb1b56d3ecb293896598de87158568218ee
Dilsha Shirin <dilshashirin81@gmail.com>
2022-11-05 21:41:21 +0530

    modified

---
+++ validate.js
@@ -0,0 +1,34 @@
+if (localStorage.getItem("isLogin") === 'true') {
+    location.replace('main.html');
+    var Backlen = history.length;
+    history.go(-Backlen);
+    window.location.href = "main.html";
+  }
+  var mail = document.getElementById("username");
+  var pass = document.getElementById("password");
+  function validate() {
+    if ((mail.value == "admin") && (pass.value == 12345)) {
+      // console.log(true);
+      success();
+  
+    } else {
+  
+      // console.log(false);
+      fail();
+    }
+  
+  }
+  
+  function success() {
+    console.log('SUCCESS');
+    // document.getElementById("login").setAttribute("action", "main.html");
+    localStorage.setItem("isLogin", true);
+    // location.replace("main.html");
+  }
+  function fail() {
+    console.log('FAIL');
+    localStorage.setItem("isLogin", false);
+   
+  }
+  
+ 
